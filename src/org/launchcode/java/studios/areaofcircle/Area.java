e9b2a3c87c1428aa34fdadc6f580b2edeeee2f9a
Jessica Zager <jmzager@gmail.com>
2021-09-02 20:47:57 -0500

    Chapter 2 Exercises Complete

---
+++ src/org/launchcode/java/studios/areaofcircle/Area.java
@@ -0,0 +1,20 @@
+package org.launchcode.java.studios.areaofcircle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a radius:");
+        Double radius = input.nextDouble();
+        //Double areaOutput = 3.14 * radius * radius;
+        Double area = Circle.getArea(radius);
+
+        System.out.println("The area of a circle of radius"+ radius + "is:" + area);
+
+        input.close();
+
+
+    }
+}
